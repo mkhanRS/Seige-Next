b03a35a0bbb67a2e9663eea85826a57167e48ec6
ashraf-rtl <skabir@relisource.com>
2022-10-06 17:45:01 +0600

    sidebar expand and icon added

--- components/layouts/Sidebar.jsx
+++ components/layouts/Sidebar.jsx
@@ -1,30 +1,36 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { BiHome } from "react-icons/bi";
+import { BiHome, BiImages } from "react-icons/bi";
+import { BsStack } from "react-icons/bs";
+import { HiUsers } from "react-icons/hi";
 
 const SIDEBAR_LINKS = [
   {
     id: "46f7dc9b-e75d-40e0-8d14-1e7334d16d5e",
     link: "/dashboard/practice-overview",
-    title: "Home",
+    title: "Dashboard",
+    slug: "dashboard",
     icon: "",
   },
   {
     id: "46f7dc9b-e75d-40e0-8d14-1e7334d16d5ee",
     link: "/",
-    title: "Home1",
+    title: "Image List",
+    slug: "image-list",
     icon: "",
   },
   {
     id: "46f7dc9b-e75d-40e0-8d14-1e7334d16d5eee",
     link: "/",
-    title: "Home2",
+    title: "Technologists List",
+    slug: "technologists-list",
     icon: "",
   },
   {
     id: "46f7dc9b-e75d-40e0-8d14-1e7334d16d5eeee",
     link: "/",
-    title: "Home3",
+    title: "test",
+    slug: "test",
     icon: "",
   },
 ];
@@ -37,16 +43,24 @@ export default function SideBar() {
     setActiveLink(link.id);
     router.push(link.link);
   }
+
+  // to expand sidebar
+  const [expandSidebar, setExpandSidebar] = useState(false);
+  const handleExpandSidebar = () => {
+    console.log("expandSidebar ", expandSidebar);
+    setExpandSidebar(!expandSidebar);
+  }
+
   return (
-    <nav
-      style={{
-        position: "fixed",
-        height: "100%",
-        width: "75px",
-        background: "var(--clr-sidebar-bg)",
-        padding: "20px 0",
-      }}
-    >
+    <nav className={!expandSidebar ? "sidebar__normal" : "sidebar__expand"}>
+
+      <div
+        className={!expandSidebar ? "sidebar__brand" : "sidebar__brand__expanded"}
+        onClick={() => handleExpandSidebar()}
+      >
+        <BsStack size={25} color="white" />
+      </div>
+      
       {SIDEBAR_LINKS.map((link) => (
         <div
           key={link.id}
@@ -55,8 +69,25 @@ export default function SideBar() {
         >
           <div className="triangle1"></div>
 
-          <div className="sidebar__link_icon">
-            <BiHome size={25} color={activeLink === link.id ? "black" : "white"} />
+          <div className={`${!expandSidebar ? "sidebar__link_icon" : "sidebar__link__icon__expanded" }`}>
+
+            {link.slug === "dashboard" ? (
+              <span style={{ display: "flex", alignItems: "center" }}>
+                <BiHome size={25} color={activeLink === link.id ? "black" : "white"} />
+                <span className={`${!expandSidebar ? "d__none" : "side__menu__text" } ${activeLink === link.id ? "side__menu__text__active" : "side__menu__text__inactive"}`}>Dashboard</span>
+              </span>
+            ) : link.slug === "image-list" ? (
+              <span style={{ display: "flex", alignItems: "center" }}>
+                <BiImages size={25} color={activeLink === link.id ? "black" : "white"} />
+                <span className={`${!expandSidebar ? "d__none" : "side__menu__text" } ${activeLink === link.id ? "side__menu__text__active" : "side__menu__text__inactive"}`}>Image List</span>
+              </span>
+            ) : link.slug === "technologists-list" ? (
+              <span style={{ display: "flex", alignItems: "center" }}>
+                <HiUsers size={25} color={activeLink === link.id ? "black" : "white"} />
+                <span className={`${!expandSidebar ? "d__none" : "side__menu__text" } ${activeLink === link.id ? "side__menu__text__active" : "side__menu__text__inactive"}`}>Technologists List</span>
+              </span>
+            ) : null}
+
           </div>
 
           <div className="triangle2"></div>
